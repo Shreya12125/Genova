739249ca50cfd82c639ea5e5ce9ab7d31eaf9bae
NandanaThachilath <nanduthachilath2004@gmail.com>
2026-01-01 10:22:32 +0530

    Update tr.py
    Remove code for loading and printing job titles from a CSV file.

--- tr.py
+++ tr.py
@@ -1,9 +1,1 @@
-import pandas as pd
 
-# Load the dataset
-file_path = r"C:\Users\Admin\Downloads\archive (1)\all_job_post.csv"
-df = pd.read_csv(file_path)
-
-# Print all job titles
-for title in df['job_title']:
-    print(title)
